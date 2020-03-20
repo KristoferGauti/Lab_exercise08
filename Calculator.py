de3a3a8065c40605d181caf72d40af951439a4f2
kristofer <kristofergauti@gmail.com>
2020-03-20 14:10:54 +0000

    Green tests for pytest with adding numbers with a new line character

--- Calculator.py
+++ Calculator.py
@@ -1,10 +1,26 @@
 class Calculator(object):
+    @staticmethod
+
     def Add(string):
-        if string == "":
+        replaced_string = ""
+        if "\n" in string:
+            replaced_string += string.replace("\n",",")
+        else:
+            replaced_string += string
+
+        if replaced_string == "":
             return 0
-        elif string.isdigit():
-            return int(string)
-        elif len(string) > 1:
-            string_list = string.split(",")
-            int_list = [int(num) for num in string_list]
+
+        elif replaced_string .isdigit():
+            return int(replaced_string )
+
+        elif len(replaced_string ) > 1:
+            replaced_string_list = replaced_string.split(",")
+            int_list = [int(num) for num in replaced_string_list]
             return sum(int_list)
+
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.Add("1\n2,3")
+
+        
--- test_Calculator.py
+++ test_Calculator.py
@@ -8,3 +8,6 @@ def test_add_the_string_one_returns_int_one():
 
 def test_add_with_two_string_numbers_seperated_with_comma():
     assert Calculator.Add("1,2")
+
+def test_add_with_two_string_numbers_seperated_with_comma_and_a_new_line():
+    assert Calculator.Add("1\n2,3")
